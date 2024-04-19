6607b0f89604a2e312db9d8ece927a6e689e5a2c
gyl30 <im.ggyyll@gmail.com>
2024-04-19 18:09:23 +0800

    Add socket

---
+++ socket.cpp
@@ -0,0 +1,156 @@
+#include <boost/system.hpp>
+
+#include "socket.h"
+#include "log.h"
+
+namespace leaf
+{
+template <typename Ed>
+std::string get_endpoint_address_(const Ed& ed)
+{
+    boost::system::error_code ec;
+    const std::string ip = ed.address().to_string(ec);
+    if (ec)
+    {
+        LOG_ERROR("get endpoint address failed {}", ec.message());
+        return "";
+    }
+    const uint16_t port = ed.port();
+    return ip + ":" + std::to_string(port);
+}
+
+template <typename Socket>
+static std::string get_socket_local_ip_(Socket& socket)
+{
+    boost::system::error_code ec;
+    auto ed = socket.local_endpoint(ec);
+    if (ec)
+    {
+        LOG_ERROR("get socket local address failed {}", ec.message());
+        return "";
+    }
+    std::string ip = ed.address().to_string(ec);
+    if (ec)
+    {
+        return "";
+    }
+    return ip;
+}
+template <typename Socket>
+static uint16_t get_socket_local_port_(Socket& socket)
+{
+    boost::system::error_code ec;
+    auto ed = socket.local_endpoint(ec);
+    if (ec)
+    {
+        LOG_ERROR("get socket local port failed {}", ec.message());
+        return 0;
+    }
+    return ed.port();
+}
+//
+template <typename Socket>
+static std::string get_socket_remote_ip_(Socket& socket)
+{
+    boost::system::error_code ec;
+    auto ed = socket.remote_endpoint(ec);
+    if (ec)
+    {
+        LOG_ERROR("get socket remote address failed {}", ec.message());
+        return "";
+    }
+    std::string ip = ed.address().to_string(ec);
+    if (ec)
+    {
+        return "";
+    }
+    return ip;
+}
+template <typename Socket>
+static uint16_t get_socket_remote_port_(Socket& socket)
+{
+    boost::system::error_code ec;
+    auto ed = socket.remote_endpoint(ec);
+    if (ec)
+    {
+        LOG_ERROR("get socket remote port failed {}", ec.message());
+        return 0;
+    }
+    return ed.port();
+}
+
+std::string get_socket_local_ip(boost::asio::ip::tcp::socket& socket) { return get_socket_local_ip_(socket); }
+uint16_t get_socket_local_port(boost::asio::ip::tcp::socket& socket) { return get_socket_local_port_(socket); }
+std::string get_socket_remote_ip(boost::asio::ip::tcp::socket& socket) { return get_socket_remote_ip_(socket); }
+uint16_t get_socket_remote_port(boost::asio::ip::tcp::socket& socket) { return get_socket_remote_port_(socket); }
+std::string get_endpoint_address(const boost::asio::ip::tcp::endpoint& ed) { return get_endpoint_address_(ed); }
+
+std::string get_socket_remote_address(boost::asio::ip::tcp::socket& socket)
+{
+    boost::system::error_code ec;
+    auto ed = socket.remote_endpoint(ec);
+    if (ec)
+    {
+        LOG_ERROR("get socket remote address failed {}", ec.message());
+        return "";
+    }
+    return get_endpoint_address(ed);
+}
+
+std::string get_socket_local_address(boost::asio::ip::tcp::socket& socket)
+{
+    boost::system::error_code ec;
+    auto ed = socket.local_endpoint(ec);
+    if (ec)
+    {
+        LOG_ERROR("get socket local address failed {}", ec.message());
+        return "";
+    }
+    return get_endpoint_address(ed);
+}
+
+std::string get_socket_local_ip(boost::asio::ip::udp::socket& socket) { return get_socket_local_ip_(socket); }
+uint16_t get_socket_local_port(boost::asio::ip::udp::socket& socket) { return get_socket_local_port_(socket); }
+std::string get_socket_remote_ip(boost::asio::ip::udp::socket& socket) { return get_socket_remote_ip_(socket); }
+uint16_t get_socket_remote_port(boost::asio::ip::udp::socket& socket) { return get_socket_remote_port_(socket); }
+std::string get_endpoint_address(const boost::asio::ip::udp::endpoint& ed) { return get_endpoint_address_(ed); }
+
+std::string get_socket_local_address(boost::asio::ip::udp::socket& socket)
+{
+    boost::system::error_code ec;
+    auto ed = socket.local_endpoint(ec);
+    if (ec)
+    {
+        LOG_ERROR("get socket local address failed {}", ec.message());
+        return "";
+    }
+    return get_endpoint_address(ed);
+}
+std::string get_socket_remote_address(boost::asio::ip::udp::socket& socket)
+{
+    boost::system::error_code ec;
+    auto ed = socket.remote_endpoint(ec);
+    if (ec)
+    {
+        LOG_ERROR("get socket remote address failed {}", ec.message());
+        return "";
+    }
+    return get_endpoint_address(ed);
+}
+
+boost::asio::ip::tcp::socket change_socket_io_context(boost::asio::ip::tcp::socket sock, boost::asio::io_context& io)
+{
+    std::string local = get_socket_local_address(sock);
+    std::string remote = get_socket_remote_address(sock);
+    boost::system::error_code ec;
+    boost::asio::ip::tcp::socket tmp(io);
+    auto fd = sock.release(ec);
+    if (ec)
+    {
+        LOG_ERROR("local {} remote {} change context failed {}", local, remote, ec.message());
+        return tmp;
+    }
+    tmp.assign(boost::asio::ip::tcp::v4(), fd);
+    return tmp;
+}
+}    // namespace leaf
